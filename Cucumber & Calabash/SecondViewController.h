b3d8cfebe33b97ffcf4bfe07b540a80db3e78754
Alberto Lagos <alberto.lagos@gmail.com>
2015-04-15 17:20:49 -0300

    Added more stuff

---
+++ Cucumber & Calabash/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Cucumber & Calabash
+//
+//  Created by Alberto Lagos on 15-04-15.
+//  Copyright (c) 2015 Alberto Lagos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ Cucumber & Calabash/SecondViewController.m
@@ -0,0 +1,37 @@
+//
+//  SecondViewController.m
+//  Cucumber & Calabash
+//
+//  Created by Alberto Lagos on 15-04-15.
+//  Copyright (c) 2015 Alberto Lagos. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
